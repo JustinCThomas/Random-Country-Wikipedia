e7804036c2c9084048ebefdcc6a3465f9524d5db
Justin Thomas <thomascjustin@gmail.com>
2021-10-05 22:53:03 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import IframeContainer from "./components/IframeContainer"
+
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <IframeContainer />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/IframeContainer.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const iframeContainerStyling = {
+  height: "100vh",
+  width: "100vw"
+};
+
+function IframeContainer(props) {
+
+  return (
+    <div className="iframe-container" style={iframeContainerStyling}>
+      <iframe src="https://en.wikipedia.org/wiki/United_States" height="70%" width="70%" title="Wikipedia page of a country"></iframe>
+    </div>
+  )
+}
+
+export default IframeContainer
