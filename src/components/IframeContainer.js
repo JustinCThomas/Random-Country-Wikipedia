1c190d48694e191e455d3f04a98a5814da42eb31
Justin Thomas <thomascjustin@gmail.com>
2021-10-06 01:06:23 -0400

    Change Iframe Size

--- src/components/IframeContainer.js
+++ src/components/IframeContainer.js
@@ -1,15 +1,15 @@
 import React from "react"
 
 const iframeContainerStyling = {
-  height: "100vh",
-  width: "100vw"
+  height: "92vh",
+  width: "95vw"
 };
 
 function IframeContainer(props) {
 
   return (
     <div className="iframe-container" style={iframeContainerStyling}>
-      <iframe src={"https://en.wikipedia.org/wiki/" + props.country} height="70%" width="70%" title="Wikipedia page of a country"></iframe>
+      <iframe src={"https://en.wikipedia.org/wiki/" + props.country} height="100%" width="100%" title="Wikipedia page of a country"></iframe>
     </div>
   )
 }
